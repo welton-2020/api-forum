ae65fb374c7dd79c45a5f9c021604a2eca2300cf
welton-2020 <weltdias@gmail.com>
2021-02-24 12:39:05 -0300

    Configurando autenticação Stateless

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$IymMIzjY1kSKjK6sVASyvO4OWOFvmNcp7SMW.cK5aDfm0FP26u56q');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
